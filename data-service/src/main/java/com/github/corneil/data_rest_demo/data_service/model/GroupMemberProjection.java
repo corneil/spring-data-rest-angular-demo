66998a9553f0ae3088e0366b3f6b8416517d3ea2
Corneil du Plessis <corneil.duplessis@gmail.com>
2016-07-22 11:56:05 +0200

    added projection

--- data-service/src/main/java/com/github/corneil/data_rest_demo/data_service/model/GroupMemberProjection.java
+++ data-service/src/main/java/com/github/corneil/data_rest_demo/data_service/model/GroupMemberProjection.java
@@ -5,9 +5,7 @@ import com.github.corneil.data_rest_demo.data_service.data.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.rest.core.config.Projection;
 
-/**
- * Created by Corneil on 2016-07-21.
- */
+
 @Projection(name = "detail", types = GroupMember.class)
 public interface GroupMemberProjection {
     @Value("#{target.group.groupName}")
--- data-service/src/main/java/com/github/corneil/data_rest_demo/data_service/repository/GroupMemberRepository.java
+++ data-service/src/main/java/com/github/corneil/data_rest_demo/data_service/repository/GroupMemberRepository.java
@@ -1,13 +1,14 @@
 package com.github.corneil.data_rest_demo.data_service.repository;
 
 import com.github.corneil.data_rest_demo.data_service.data.GroupMember;
+import com.github.corneil.data_rest_demo.data_service.model.GroupMemberProjection;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
-@RepositoryRestResource(collectionResourceRel = "members", itemResourceRel = "member", path = "members")
+@RepositoryRestResource(collectionResourceRel = "members", itemResourceRel = "member", path = "members", excerptProjection = GroupMemberProjection.class)
 public interface GroupMemberRepository extends CrudRepository<GroupMember, Long> {
     List<GroupMember> findByGroup_GroupName(@Param("groupName") String groupName);
     List<GroupMember> findByGroup_GroupNameAndEnabledTrue(@Param("groupName") String groupName);
