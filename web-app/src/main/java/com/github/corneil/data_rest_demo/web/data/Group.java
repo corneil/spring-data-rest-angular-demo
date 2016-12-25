1bcaf2af2e79a3a6e2b2c9343f7d12244e0aced1
Corneil du Plessis <corneil@jumpco.io>
2016-12-25 06:47:02 +0200

    bump versions

--- web-app/src/main/java/com/github/corneil/data_rest_demo/web/data/Group.java
+++ web-app/src/main/java/com/github/corneil/data_rest_demo/web/data/Group.java
@@ -10,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -29,6 +30,7 @@ public class Group {
     private String groupName;
     @NotNull
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "GROUP_OWNER")
     private User groupOwner;
     @Id
     @GeneratedValue
